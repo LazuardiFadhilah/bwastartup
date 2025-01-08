9f0dd348b35184d65a5bdfe1462c0be406f03709
Lazuardi Fadhilah <Lazuardifadhillah@gmail.com>
2025-01-08 21:42:41 +0700

    initialize Go module and add user management structure

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bwastartup/user"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	// dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
+
+	// fmt.Println("Connection Success")
+
+	// var users []user.User
+
+	// db.Find(&users)
+
+	// for _, user := range users {
+	// 	fmt.Println(user.Name)
+	// 	fmt.Println(user.Email)
+	// 	fmt.Println("----------------")
+	// }
+
+	router := gin.Default()
+	router.GET("/handler", handler)
+
+	router.Run()
+}
+
+func handler(c *gin.Context) {
+	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	var users []user.User
+	db.Find(&users)
+	fmt.Println(users)
+
+	c.JSON(http.StatusOK, users)
+
+}
