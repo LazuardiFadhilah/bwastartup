4a21834ad4f6caf3ce0ee407930a083225656871
Lazuardi Fadhilah <Lazuardifadhillah@gmail.com>
2025-01-09 17:13:49 +0700

    implement user registration service and repository pattern

--- main.go
+++ main.go
@@ -4,40 +4,12 @@ import (
 	"bwastartup/user"
 	"fmt"
 	"log"
-	"net/http"
 
-	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	// dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
-	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-	// if err != nil {
-	// 	log.Fatal(err.Error())
-	// }
-
-	// fmt.Println("Connection Success")
-
-	// var users []user.User
-
-	// db.Find(&users)
-
-	// for _, user := range users {
-	// 	fmt.Println(user.Name)
-	// 	fmt.Println(user.Email)
-	// 	fmt.Println("----------------")
-	// }
-
-	router := gin.Default()
-	router.GET("/handler", handler)
-
-	router.Run()
-}
-
-func handler(c *gin.Context) {
 	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -45,10 +17,15 @@ func handler(c *gin.Context) {
 		log.Fatal(err.Error())
 	}
 
-	var users []user.User
-	db.Find(&users)
-	fmt.Println(users)
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
 
-	c.JSON(http.StatusOK, users)
+	userInput := user.RegisterUserInput{}
+	userInput.Name = "Test Simpan dari Service"
+	userInput.Occupation = "Software Engineer"
+	userInput.Email = "contoh@gmail.com"
+	userInput.Password = "password"
 
+	userService.RegisterUser(userInput)
+	fmt.Println(userInput)
 }
